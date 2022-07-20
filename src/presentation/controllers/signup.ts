ce0f76787352cc6eef069037e77f7a7415b9b063
Rodrigues <fr.costa95@gmail.com>
2022-07-20 18:19:25 -0300

    feat(protocols): create interface to type http request and response of singupController

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,12 @@
+import { HttpRequest, HttpResponse } from '../protocols/http';
+
 export class SignUpController {
-  handle(httpRequest: any): any {
-    return { statusCode: 400, body: new Error('Missing param: name') };
+  handle(httpRequest: HttpRequest): HttpResponse {
+    let body;
+
+    if (!httpRequest.body.name) body = new Error('Missing param: name');
+    if (!httpRequest.body.email) body = new Error('Missing param: email');
+
+    return { statusCode: 400, body };
   }
 }
