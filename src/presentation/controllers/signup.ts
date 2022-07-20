d93b69de4e9129135ac620b94e1530d24ebae2ec
Rodrigues <fr.costa95@gmail.com>
2022-07-20 20:53:47 -0300

    chore(jest): ignore protocols from coverage report

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,11 +1,11 @@
 import { HttpRequest, HttpResponse } from '../protocols/http';
-
+import { MissingParamError } from '../errors/missing-param-error';
 export class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
     let body;
 
-    if (!httpRequest.body.name) body = new Error('Missing param: name');
-    if (!httpRequest.body.email) body = new Error('Missing param: email');
+    if (!httpRequest.body.name) body = new MissingParamError('name');
+    if (!httpRequest.body.email) body = new MissingParamError('email');
 
     return { statusCode: 400, body };
   }
--- src/presentation/protocols/http.ts
+++ src/presentation/protocols/http.ts
@@ -1,3 +1,5 @@
+/* istanbul ignore file */
+
 export interface HttpRequest {
   body?: any;
 }
